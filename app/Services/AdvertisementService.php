7627b6ebd761558df290790630c43ebe7c626aac
sunguide <sunguide@qq.com>
2018-07-26 02:04:19 +0800

    添加广告模块

---
+++ app/Services/AdvertisementService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+use App\Models\Advertisement\Position;
+
+class AdvertisementService
+{
+    public function getPositionItems(Position $position)
+    {
+        return $position->with(['productSku.product'])->where('position_id', $position->id)->first();
+    }
+
+    public function addPosition($name, $content, $displayMode, $status)
+    {
+        $position = new Position(['name' => $name, 'content' => $content,
+            'display_mode' => $displayMode, 'status' => $status]);
+        $position->save();
+        return $position;
+    }
+
+    public function removePositon($positionIds)
+    {
+        if (!is_array($positionIds)) {
+            $positionIds = [$positionIds];
+        }
+        $position =  new Position();
+        $position->whereIn('product_sku_id', $positionIds)->delete();
+    }
+}
